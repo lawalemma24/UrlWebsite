796ebad9d8df4046243dd4dc1b1e783b6b793bd8
lawalemma24 <lawalemma24@gmail.com>
2026-01-11 07:25:26 +0100

    ejhebh

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ app.use(cors({
 
 
 app.options('*', (req, res) => {
-    res.header('Access-Control-Allow-Origin', 'https://url-website-p59k.vercel.app');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
